25404d6410a76d7a141fa4b3d7322e938d86de43
RickAck <38399222+RickAck@users.noreply.github.com>
2018-04-15 14:44:17 +0200

    Commit3
    Added a comment :)

--- CodeSnippets.java
+++ CodeSnippets.java
@@ -1,6 +1,10 @@
 
 public class CodeSnippets {
 
+
+// Checks if an integer contains a specific number
+// For exempel (22473251, 73) will return true 
+// (22473251, 74) will return false
 public static boolean containsNumber(int i, int num)
 {
        if(i == num)
